76299b00550ce74e3fd3645e202555f3db74aecd
franklindias <franklindias99@gmail.com>
2016-01-25 00:53:57 -0200

    send email, gitignore, error page, success page

--- designon/core/views.py
+++ designon/core/views.py
@@ -1,4 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from datetime import datetime   
+from django.core import mail
 
 def home(request):
+    if request.method == 'POST':
+        
+        servicos = ''
+        
+        for serv in request.POST.getlist('servico'):
+            servicos += serv+', '
+            
+        object = {'nome':request.POST['name'],
+                  'email':request.POST['email'],
+                  'ambiente':request.POST['tipo_ambiente'],
+                  'tamanho':request.POST['size'],
+                  'obs':request.POST['message'],
+                  'servicos':servicos,
+                  'data_solicitacao':datetime.now()}
+        
+        body = render_to_string('orcamentos/solicitacao_orcamento.txt', object)
+        
+        mail.send_mail('Solicitação de Orçamento',
+                       body,
+                       request.POST['name']+' <'+request.POST['email']+'>',
+                       ['designon.arq@gmail.com'],
+                       html_message=body
+                      )
+        
+        body = render_to_string('orcamentos/agradecimento.txt', object)
+        
+        mail.send_mail('[DesignOn] - Obrigado!',
+                       body,
+                       'DesignOn <designon.arq@gmail.com>',
+                       [request.POST['email']],
+                       html_message=body
+                      )
+        
+        return render(request, 'sucesso.html')
+    
     return render(request, 'index.html')
--- designon/settings.py
+++ designon/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['designon.herokuapp.com', 'designon.arq.br', 'www.designon.arq.br']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS')
 
 
 # Application definition
@@ -103,11 +103,21 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+#email configuration
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
